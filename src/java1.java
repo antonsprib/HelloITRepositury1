144b362814bbbea23f0cf17d302ddc55918294c1
Antons <antonsprib@gmail.com>
2018-10-04 20:54:28 +0300

    lks

--- src/java1.java
+++ src/java1.java
@@ -2,5 +2,6 @@ public class java1 {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("asd");
+        System.out.println("asd");
     }
 }
